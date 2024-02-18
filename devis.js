a048b30ac1c317a35f24de65e4b6227393efe0f8
corentin <corentin.girard@datadoghq.com>
2024-02-18 21:57:34 +0100

    Improve devis

--- devis.js
+++ devis.js
@@ -1,6 +1,6 @@
 function gererAffichageChanteurs() {
-  var pisteVoix = document.getElementById("pisteVoix").value;
-  var ligneNombreChanteurs = document.getElementById("ligneNombreChanteurs");
+  const pisteVoix = document.getElementById("pisteVoix").value;
+  const ligneNombreChanteurs = document.getElementById("ligneNombreChanteurs");
 
   if (pisteVoix === "oui") {
     ligneNombreChanteurs.style.display = "block";
@@ -9,49 +9,46 @@ function gererAffichageChanteurs() {
   }
 }
 
-function calculerDevis() {
-  // Obtenez les valeurs sélectionnées par l'utilisateur
-  var genre = document.getElementById("genre").value;
-  var duree = parseFloat(document.getElementById("duree").value);
-  var nombrePistes = parseInt(document.getElementById("nombrePistes").value);
-  var qualiteMix = parseFloat(document.getElementById("qualiteMix").value);
-  var pisteVoix = document.getElementById("pisteVoix").value;
-  var nombreChanteurs = parseInt(
-    document.getElementById("nombreChanteurs").value
+function calculerPrix() {
+  const genre = document.getElementById("genre").value;
+  const duree = parseFloat(document.getElementById("duree").value);
+  const nombrePistes = parseInt(document.getElementById("nombrePistes").value);
+  const qualiteMix = parseFloat(document.getElementById("qualiteMix").value);
+  const pisteVoix = document.getElementById("pisteVoix").value;
+  const nombreChanteurs = parseInt(
+    document.getElementById("nombreChanteurs").value,
   );
 
-  // Effectuez le calcul du devis en fonction des paramètres
-  var tarifGenre = 2; // Tarif par défaut
+  // Calcul du devis en fonction des paramètres
+  let tarifGenre = 2; // Tarif par défaut
   if (genre === "pop" || genre === "hip-hop") {
     tarifGenre = 0.15;
   }
 
-  var devis =
+  let devis =
     duree * 2 + nombrePistes * 2 + qualiteMix * 5 * duree + tarifGenre * duree;
 
-  if (pisteVoix === "oui") {
-    document.getElementById("ligneNombreChanteurs").style.display = "block";
-    // Vérifiez si le nombre de chanteurs est supérieur à 0
-    if (nombreChanteurs > 0) {
-      devis += nombreChanteurs * 4 * duree; // Supplément par chanteur par minute
-    }
-  } else {
-    document.getElementById("ligneNombreChanteurs").style.display = "none";
+  if (pisteVoix === "oui" && nombreChanteurs > 0) {
+    devis += nombreChanteurs * 4 * duree; // Supplément par chanteur par minute
   }
 
-  // Affichez le devis en temps réel
-  document.getElementById("devis").textContent = devis + " €";
+  return devis;
+}
 
-  // Afficher le bouton "Envoyer mon devis à Thibault_Ecta"
-  document.getElementById("boutonEnvoyerDevis").style.display = "block";
+function calculerDevis() {
+  // Affichage du prix en temps réel
+  const prix = calculerPrix();
+  document.getElementById("devis").style.display = "block";
+  document.getElementById("prix").textContent = prix + " €";
 }
 
 function envoyerDevis() {
-  // Récupérez le devis calculé
-  var devis = document.getElementById("devis").textContent;
-  // Ouvrez un nouvel e-mail prérempli avec le destinataire et le sujet
-  var emailLink =
-    "mailto:thibaultgirard297@gmail.com?subject=Devis Musique&body=Devis%20calculé%3A%20" +
-    devis;
+  const prix = calculerPrix();
+
+  const emailBody = `Prix du devis calculé: ${prix} €`;
+
+  // E-mail prérempli avec le destinataire et le sujet.
+  const emailLink = `mailto:thibaultgirard297@gmail.com?subject=Devis Musique&body=${encodeURIComponent(emailBody)}`;
+
   window.open(emailLink, "_blank"); // Ouvre un nouvel onglet
 }
